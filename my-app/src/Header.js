72603a4d5bbae7f30c2d9d890574ec092ae66a95
Zach Estus <zachary.estus@student.awesomeinc.org>
2022-11-23 20:18:26 +0000

    Navbar and header

---
+++ my-app/src/Header.js
@@ -0,0 +1,8 @@
+export default function Header(props){
+    return(
+      <header className="row d-flex container-fluid flex-row"> 
+          <h1 className="d-flex col-6 text-white justify-content-start">Zachary Estus Showcase</h1>
+          <h1 className="d-flex col-6 text-white justify-content-end">Code Wars: IV A New Hire</h1>
+      </header>
+    );
+  }
---
+++ my-app/src/navbar.js
@@ -0,0 +1,35 @@
+import './css/styles.css'
+
+export default function Navbar(props){
+  return(
+      <nav className="navbar navbar-expand-lg secondary-color">
+          <div className="container-fluid">
+            <a className="navbar-brand fw-bold accent-text">Zestus</a>
+            <button className="navbar-toggler primary-color" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar navbar-collapse" id="navbarSupportedContent">
+              <ul className="navbar-nav d-flex ">
+                <li className="nav-item">
+                  <a className="m-1 button btn topnav accent nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li className="nav-item">
+                  <a className="m-1 nav-link button btn topnav accent">Projects</a>
+                </li>
+                <li className="nav-item">
+                  <a className="m-1 nav-link button btn topnav accent" href="https://github.com/Zestus01" target='_blank'>Github</a>
+                </li>
+                <li className="nav-item">
+                  <a className="m-1 nav-link button btn topnav accent" href="#">Blogs</a>
+                </li>
+                <li className="nav-item">
+                  <a className="m-1 nav-link button btn topnav accent" href="https://drive.google.com/file/d/1Bm5uyAgVFSUrIeB5YFKuxQCsftnSfiL3/view?usp=sharing" target="_blank">Resume</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+  );
+}
+
+
